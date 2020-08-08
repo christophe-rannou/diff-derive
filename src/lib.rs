69ee46b6aa1412d94493be6c2336841c32b5845c
BenHall-7 <benjaminjahall@gmail.com>
2020-08-08 01:25:18 -0400

    Allow passing attributes to generated struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
 extern crate proc_macro;
 use crate::proc_macro::TokenStream;
-use quote::{quote, format_ident};
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Field, Ident};
-use syn::punctuated::Punctuated;
+use quote::{format_ident, quote};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::{Punctuated};
 use syn::token::Comma;
+use syn::{
+    parenthesized, parse, parse_macro_input, Attribute, Data, DeriveInput, Error, Field, Fields, Ident,
+};
 
-#[proc_macro_derive(Diff)]
+#[proc_macro_derive(Diff, attributes(diff))]
 pub fn diff_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+    let struct_attr = parse_struct_attributes(&input.attrs);
 
     match input.data {
         Data::Struct(data_struct) => {
             let name = &input.ident;
-            let diff_name = format_ident!("{}Diff", name);
+            let diff_name = &struct_attr.name.unwrap_or(format_ident!("{}Diff", name));
+            let attr = &struct_attr.attrs.0;
 
             let (fields, is_named) = match &data_struct.fields {
                 Fields::Named(fields) => (&fields.named, true),
@@ -21,20 +26,20 @@ pub fn diff_derive(input: TokenStream) -> TokenStream {
             };
 
             if is_named {
-                derive_named(name, &diff_name, fields)
+                derive_named(attr, name, diff_name, fields)
             } else {
-                derive_unnamed(name, &diff_name, fields)
+                derive_unnamed(attr, name, diff_name, fields)
             }
         }
         _ => todo!(),
     }
 }
 
-fn derive_named(name: &Ident, diff_name: &Ident, fields: &Punctuated<Field, Comma>) -> TokenStream {
+fn derive_named(attr: &[Attribute], name: &Ident, diff_name: &Ident, fields: &Punctuated<Field, Comma>) -> TokenStream {
     let names = fields.iter().map(|field| &field.ident).collect::<Vec<_>>();
     let types = fields.iter().map(|field| &field.ty).collect::<Vec<_>>();
     (quote! {
-        #[derive(Debug, PartialEq)]
+        #(#attr)*
         pub struct #diff_name {
             #(pub #names: <#types as Diff>::Repr),*
         }
@@ -58,14 +63,20 @@ fn derive_named(name: &Ident, diff_name: &Ident, fields: &Punctuated<Field, Comm
                 }
             }
         }
-    }).into()
+    })
+    .into()
 }
 
-fn derive_unnamed(name: &Ident, diff_name: &Ident, fields: &Punctuated<Field, Comma>) -> TokenStream {
+fn derive_unnamed(
+    attr: &[Attribute],
+    name: &Ident,
+    diff_name: &Ident,
+    fields: &Punctuated<Field, Comma>,
+) -> TokenStream {
     let (numbers, types): (Vec<_>, Vec<_>) =
         fields.iter().map(|field| &field.ty).enumerate().unzip();
     (quote! {
-        #[derive(Debug, PartialEq)]
+        #(#attr)*
         pub struct #diff_name (
             #(pub <#types as Diff>::Repr),*
         )
@@ -89,5 +100,64 @@ fn derive_unnamed(name: &Ident, diff_name: &Ident, fields: &Punctuated<Field, Co
                 )
             }
         }
-    }).into()
+    })
+    .into()
 }
+
+#[derive(Default)]
+struct StructAttributes {
+    name: Option<Ident>,
+    visible: bool,
+    attrs: OuterAttributes,
+}
+
+/// A named attribute with unspecified tokens inside parentheses
+struct ParenAttr {
+    name: Ident,
+    tokens: TokenStream,
+}
+
+impl Parse for ParenAttr {
+    fn parse(input: ParseStream) -> Result<Self, Error> {
+        let name = input.parse()?;
+        let content;
+        parenthesized!(content in input);
+        Ok(ParenAttr {
+            name,
+            tokens: content.parse::<proc_macro2::TokenStream>()?.into(),
+        })
+    }
+}
+
+#[derive(Default)]
+struct OuterAttributes(Vec<Attribute>);
+
+impl Parse for OuterAttributes {
+    fn parse(input: ParseStream) -> Result<Self, Error> {
+        Ok(Self(input.call(Attribute::parse_outer)?))
+    }
+}
+
+fn parse_struct_attributes(attrs: &[Attribute]) -> StructAttributes {
+    let mut struct_attrs = StructAttributes::default();
+    attrs
+        .iter()
+        .filter(|a| a.path.is_ident("diff"))
+        .for_each(|a| {
+            let attr_named: ParenAttr = a.parse_args().unwrap();
+            let name = attr_named.name.to_string();
+            match name.as_ref() {
+                "attr" => {
+                    struct_attrs.attrs = parse(attr_named.tokens).unwrap();
+                }
+                _ => panic!("Unexpected name for diff attribute '{}'. Possible names: 'attr'", name),
+            }
+        });
+    struct_attrs
+}
+
+// fn parse_field_attributes(attrs: &[Attribute]) -> FieldAttributes {
+//     let field_attrs = FieldAttributes::default();
+//     // iter
+//     field_attrs
+// }
